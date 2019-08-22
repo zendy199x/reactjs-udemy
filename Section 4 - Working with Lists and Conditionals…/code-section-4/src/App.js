02f102173c7e0d66bd3841f3ea703e31fe12cabe
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-22 18:44:09 +0700

    53. Handling Dynamic Content "The JavaScript Way"…

--- Section 4 - Working with Lists and Conditionals…/code-section-4/src/App.js
+++ Section 4 - Working with Lists and Conditionals…/code-section-4/src/App.js
@@ -48,6 +48,32 @@ class App extends Component {
             padding: '8px',
             cursor: 'pointer'
         }
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+            <div>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    changed={this.switchNameHandler}
+                >
+                My Hobbies: Racing
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                />
+            </div>
+            )
+        }
+
         return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
@@ -55,27 +81,7 @@ class App extends Component {
             <button
                 style={style}
                 onClick={this.togglePersonsHandler}>Toggle Persons</button>
-            {
-                this.state.showPersons === true ? 
-                    <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age}
-                        />
-                        <Person
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this, 'Max!')}
-                            changed={this.switchNameHandler}
-                        >
-                        My Hobbies: Racing
-                        </Person>
-                        <Person
-                            name={this.state.persons[2].name}
-                            age={this.state.persons[2].age}
-                        />
-                    </div> : null
-            }
+            {persons}
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
