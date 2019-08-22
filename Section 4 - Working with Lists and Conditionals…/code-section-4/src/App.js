d17e320536993a41be168bbc73ef10b7f8b2677a
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-23 04:29:06 +0700

    59. Flexible Lists…

--- Section 4 - Working with Lists and Conditionals…/code-section-4/src/App.js
+++ Section 4 - Working with Lists and Conditionals…/code-section-4/src/App.js
@@ -1,78 +1,87 @@
-    import React, { Component } from 'react';
-    import './App.css';
-    import Person from './Person/Person';
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
     state = {
         persons: [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 26 }
+        { id: 'asfa1', name: 'Max', age: 28 },
+        { id: 'vasdf1', name: 'Manu', age: 29 },
+        { id: 'asdf11', name: 'Stephanie', age: 26 }
         ],
         otherState: 'some other value',
         showPersons: false
     }
 
-    nameChangeHandler = (event) => {
-        this.setState({
-            persons: [
-                { id: 'abc', name: 'Max', age: 28 },
-                { id: 'cdf', name: event.target.value, age: 29 },
-                { id: 'ghi', name: 'Stephanie', age: 27 }
-            ]
-        })
+    nameChangedHandler = ( event, id ) => {
+        const personIndex = this.state.persons.findIndex(p => {
+        return p.id === id;
+        });
+
+        const person = {
+        ...this.state.persons[personIndex]
+        };
+
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState( {persons: persons} );
     }
 
     deletePersonHandler = (personIndex) => {
         // const persons = this.state.persons.slice();
         const persons = [...this.state.persons];
         persons.splice(personIndex, 1);
-        this.setState({persons: persons})
+        this.setState({persons: persons});
     }
 
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
-        this.setState({showPersons: !doesShow})
+        this.setState( { showPersons: !doesShow } );
     }
 
-    render() {
+    render () {
         const style = {
             backgroundColor: 'white',
             font: 'inherit',
-            boder: '1px solid blue',
+            border: '1px solid blue',
             padding: '8px',
             cursor: 'pointer'
-        }
+        };
 
         let persons = null;
 
-        if (this.state.showPersons) {
-            persons = (
-                <div>
-                    {this.state.persons.map((person, index) => {
-                        return <Person
-                            click={() => this.deletePersonHandler(index)}
-                            name={person.name}
-                            age={person.age}
-                            key={person.id}
-                        />
-                    })}
-                </div>
-            )
-        }
+    if ( this.state.showPersons ) {
+        persons = (
+            <div>
+                {this.state.persons.map((person, index) => {
+                    return <Person
+                    click={() => this.deletePersonHandler(index)}
+                    name={person.name} 
+                    age={person.age}
+                    key={person.id}
+                    changed={(event) => this.nameChangedHandler(event, person.id)} />
+                })}
+            </div>
+        );
+    }
 
-        return (
+    return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
             <button
-                style={style}
-                onClick={this.togglePersonsHandler}>Toggle Persons</button>
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
             {persons}
-            </div>
+        </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
 }
 
-    export default App;
+export default App;
