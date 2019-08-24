b352db49ff9c9b9dac1aa306ab5ccab75e419595
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-24 12:09:09 +0700

    80. Using Error Boundaries (React 16+)…

--- Section 6 - Debugging React Apps…/code-section-6/src/Person/Person.js
+++ Section 6 - Debugging React Apps…/code-section-6/src/Person/Person.js
@@ -3,6 +3,13 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = ( props ) => {
+    const person =( props ) => {
+        const rnd = Math.random();
+
+        if (rnd > 0.7 ) {
+            throw new Error( 'Something went wrong' );
+        }
+    }
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
