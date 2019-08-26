c3d25edf2629174b3a0d2cec28581c59cf5394f9
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-08-26 15:07:10 +0700

    97. Optimizing Functional Components with React.memo()…

--- Section 7 - Diving Deeper into Components & ReactInternals…/code-section-7/src/containers/App.js
+++ Section 7 - Diving Deeper into Components & ReactInternals…/code-section-7/src/containers/App.js
@@ -100,7 +100,7 @@ class App extends Component {
         {this.state.showCockpit ? <Cockpit
             title={this.props.appTitle}
             showPersons={this.state.showPersons}
-            persons={this.state.persons}
+            personsLength={this.state.persons.length}
             clicked={this.togglePersonsHandler}
             /> : null }
             {persons}
